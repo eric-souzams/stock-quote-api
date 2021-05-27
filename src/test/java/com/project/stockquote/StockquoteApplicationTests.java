99b91387f2c68b55c39471d7a0a45308c21da317
Eric Magalhaes <15831837+eric-souzams@users.noreply.github.com>
2021-05-27 15:12:37 -0300

    Create project

---
+++ src/test/java/com/project/stockquote/StockquoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.stockquote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockquoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
